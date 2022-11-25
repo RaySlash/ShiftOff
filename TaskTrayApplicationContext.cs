ed3594c21215546e3eefb7d55da0b661cfa66f86
RaySlash <stevemathewjoy@gmail.com>
2022-11-26 08:25:57 +1000

    update

--- TaskTrayApplicationContext.cs
+++ TaskTrayApplicationContext.cs
@@ -9,10 +9,37 @@ using static System.Windows.Forms.DataFormats;
 
 namespace TaskTrayApplication
 {
+    public static class Prompt
+    {
+        // Dialog box for user-input project code.
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label dialogTextLabel = new() { Left = 50, Top = 20, Text = text };
+            TextBox dialogTextBox = new() { Left = 50, Top = 50, Width = 400 };
+            Button dialogConfirmation = new() { Text = "Start", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            dialogConfirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(dialogTextBox);
+            prompt.Controls.Add(dialogConfirmation);
+            prompt.Controls.Add(dialogTextLabel);
+            prompt.AcceptButton = dialogConfirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? dialogTextBox.Text : "";
+        }
+    }
     public class TaskTrayApplicationContext : ApplicationContext
     {
         public NotifyIcon notifyIcon = new();
         public Configuration configWindow = new();
+        public string startMenuText = "Start";
+        public List<string> projectCode = new();
 
         public TaskTrayApplicationContext()
         {
@@ -21,15 +48,24 @@ namespace TaskTrayApplication
             notifyIcon.ContextMenuStrip = new ContextMenuStrip() ;
             notifyIcon.ContextMenuStrip.Items.AddRange(new ToolStripItem[]
             {
+                // Variable text for Start or Stop Event
+                new ToolStripMenuItem(startMenuText ,null , new EventHandler(Start), startMenuText),
                 new ToolStripMenuItem("Configuration",null , new EventHandler(ShowConfig), "Configuration"),
                 new ToolStripMenuItem("Exit",null , new EventHandler(Exit), "Exit")
             });
             notifyIcon.Visible = true;
         }
 
+        private void Start(object sender, EventArgs e)
+        {
+            Prompt.ShowDialog("Start", @"Enter your project code(s):");
+            dialogTextBox.Text
+            projectCode.Add();
+            startMenuText = "Stop";
+        }
         private void ShowMessage(object sender, EventArgs e)
         {
-            if (TaskTrayApplication.Properties.Settings.Default.ShowMessage)
+            if (Settings.Default.ShowMessage)
                 MessageBox.Show("Hello World");
         }
 
